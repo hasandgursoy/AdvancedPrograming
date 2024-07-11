ea4ca331ced8b5dafa03422b09403fdc93c479f8
hasandgursoy <hasan.d.gursoy@gmail.com>
2024-07-12 01:00:21 +0300

    1-Arrays

---
+++ 1-Arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1 . Multi Dimensional Arrays = new int[5,2,3,5] 
+// 2.  Jagged Array = new [5][][]
+
+int[] d1 = (int[]) Array.CreateInstance(typeof(int),2);
+int[,] d2 = (int[,]) Array.CreateInstance(typeof(int),2,3);
+
+System.Console.WriteLine();
+
+(string s, int i, bool b)[] d3 = new (string s, int i, bool b)[]{
+
+    ("asdasd",123,true)
+};
+
+System.Console.WriteLine();
